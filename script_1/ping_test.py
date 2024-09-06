b9590d114bdeabb3f60a4fcf6f2f17bfff89d865
student <student@localhost.localdomain>
2024-09-06 12:42:38 -0400

    added tests for local, remote, and dns

--- script_1/ping_test.py
+++ script_1/ping_test.py
@@ -1,60 +1,81 @@
 #!:/usr/bin/python3
 # Kaiyu Kosanouvong / NSSA221 - Script 1
 # runs a command line interface that performs a variety of network functions
+import os
+import netifaces
+import subprocess
 
 # returns the default gateway of the system
 def defaultGate():
+    # Week 3 Scripting Review Code (Slide 19)
     try:
-        import netifaces
+        # get gateway via netifaces
         gws = netifaces.gateways()
-        DEFGW = hws['default'][netifaces.AF_INET][0]
-    except:
-        DEFGW = os.popen("ip r |grep '^default' |awk '{print $3}'").read().strip("\n")
+        def_GW = gws['default'][netifaces.AF_INET][0]
+    except: # 
+        def_GW = os.popen("ip r |grep '^default' |awk '{print $3}'").read().strip("\n")
 
-# tests the local connection and returns a boolean
+    return def_GW
+
+# tests the local connection and returns results
 def localConnect():
+    # get local IP (default gateway)
+    local_IP = defaultGate()
 
+    # get ping test results from 
+    pingtest = os.popen("ping -c 2 " + local_IP).read().strip("\n")
+    return pingtest
+    
 
-# test remote connection and returns a boolean
+# test remote connection and returns results
 def remoteConnect():
+    # get remote IP (RIT server)
+    remote_IP = "129.21.3.17"
 
+    # get ping test results from 
+    pingtest = os.popen("ping -c 2 " + remote_IP).read().strip("\n")
+    return pingtest
 
-# tests the dns resolution
+# tests the dns resolution and returns results
 def dnsResolution():
+    # get DNS IP (RIT server)
+    DNS_IP = "8.8.8.8"
 
-
-# exits the program
-def exit():
-
+    # get ping test results from 
+    pingtest = os.popen("ping -c 2 " + DNS_IP).read().strip("\n")
+    return pingtest
 
 # sets up a command line menu for the user
 def menu():
-    str userInput = input("Please enter a command: \n" +
-                      "(1) to get default gateway\n" +
-                      "(2) to test local connection\n" +
-                      "(3) to test remote connection\n" +
-                      "(4) to test DNS resolution\n" +
-                      "(5) to exit the program\n" +
-                      "")
-    
-    match userInput:
-        case "1":
-            defaultGate()
-            menu()
-        case "2":
-            localConnect()
-            menu()
-        case "3":
-            remoteConnect()
-            menu()
-        case "4":
-            dnsResolution()
-            menu()
-        case "5":
+    while True:
+        userInput = input("Please enter a command: \n" +
+                        "(1) to get default gateway\n" +
+                        "(2) to test local connection\n" +
+                        "(3) to test remote connection\n" +
+                        "(4) to test DNS resolution\n" +
+                        "(5) to exit the program\n" +
+                        "")
+        
+        if userInput == "1": # returns default gateway 
+            def_GW = defaultGate()
+            print("Your default gateway: " + def_GW + "\n")
+        elif userInput == "2": # runs local connection test
+            pingTest = localConnect()
+            print("Local Test results: " + pingTest)
+        elif userInput == "3": # runs remote connection test
+            pingTest = remoteConnect()
+            print("Remote Test results: " + pingTest)
+        elif userInput == "4": # runs dns resolution test
+            pingTest = dnsResolution()
+            print("DNS Test results: " + pingTest)
+        elif userInput == "5": # exits program
+            print("bye!")
             exit()
-        case _:
-            menu()
+        else:
+            print("invalid command!\n")
 
 def main():
     menu()
-    
+
+if __name__ == '__main__':
+    main()
